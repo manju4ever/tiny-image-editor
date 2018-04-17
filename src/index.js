9e18524d9ae2d8b49654d902d21fc599948af1b3
manju4ever <dmanjunath@live.com>
2018-04-18 03:01:28 +0530

    Remove console.log

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from  'react';
 import ReactDOM from 'react-dom';
 import styles from "./App.css";
 
-console.log(styles);
-
 const App = (props) => <div className={styles.root}>
         <h2> Hello { props.name ? props.name : 'World' } ! </h2>
 </div>;
